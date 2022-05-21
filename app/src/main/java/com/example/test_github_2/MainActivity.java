1cbbc7e2293474d9b9a99c178ec8d4444a6719c2
android <barsherkat@gmail.com>
2022-05-21 10:22:02 +0430

    three

--- app/src/main/java/com/example/test_github_2/MainActivity.java
+++ app/src/main/java/com/example/test_github_2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.test_github_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int a=4+9;
+        Log.e("TAG",String.valueOf(a));
+
+
     }
 }
